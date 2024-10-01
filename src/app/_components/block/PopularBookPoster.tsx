726422757952611f80183af0a95154a16c5173ca
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-01 20:07:49 -0300

    ✨ Feat: construct component of popular book

---
+++ src/app/_components/block/PopularBookPoster.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { type PopularBook } from "~/constants/popularBooks";
+import Rating from "/public/images/rating.png";
+
+export function PopularBookPoster({ book }: { book: PopularBook }) {
+  return (
+    <div className="flex min-h-[8.125rem] w-full flex-col bg-gray-700 px-5 py-4 rounded-lg">
+      <div className="flex gap-5">
+        <Image
+          width={64}
+          height={94}
+          src={book.coverImage}
+          alt="Cover image of books"
+        />
+        <div className="flex flex-col justify-between gap-5">
+          <div className="flex flex-col">
+            <strong className="text-lg text-gray-100">{book.title}</strong>
+            <span className="text-sm text-gray-400">{book.author}</span>
+          </div>
+          <Image src={Rating} alt="Ilustrative rating" />
+        </div>
+      </div>
+    </div>
+  );
+}
