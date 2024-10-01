c41de270e0e632adbbffb344e466897aded28607
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-01 18:17:49 -0300

    💄 Style: limit header into a maximum height of screen

--- src/app/_components/ui/Header.tsx
+++ src/app/_components/ui/Header.tsx
@@ -11,7 +11,7 @@ export function Header() {
   const { user } = useUser()
 
   return (
-    <header className="m-5 flex min-h-screen w-[14.5rem] flex-col items-center justify-between overflow-hidden rounded-xl bg-gray-700 pb-6 pt-12">
+    <header className="m-5 flex max-h-screen w-[14.5rem] flex-col items-center justify-between overflow-hidden rounded-xl bg-gray-700 pb-6 pt-12">
       <div className="flex flex-col gap-16">
         <div className="flex gap-2">
           <Image src={bookwiseLogo} alt="Bookwise Logo" />
